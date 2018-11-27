b8438b58736ae7b789d9d692bfc6c671a8a11941
wailabualela <wailabualela@gmail.com>
2018-11-27 11:14:19 +0200

    add detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'products', loadChildren: './products/products.module#ProductsPageModule' }
+  { path: 'products', loadChildren: './products/products.module#ProductsPageModule' },
+  { path: 'detail', loadChildren: './products/detail/detail.module#DetailPageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
